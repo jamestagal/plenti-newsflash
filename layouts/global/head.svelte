04a611ba0735bba9364a13069dc159701c544271
Benjamin Waller <benjaminjameswaller@gmail.com>
2024-04-08 19:11:43 +1000

    update baseurl

--- layouts/global/head.svelte
+++ layouts/global/head.svelte
@@ -7,7 +7,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width,initial-scale=1.0" />
   <title>Completed - NewsFlash | Home</title>
-  <base href="{ env.baseurl }">
+  <base href={env.baseurl}>
   <!-- Favicon -->
   <link
     rel="icon"
